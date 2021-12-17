6bebb5851b755a428c72511f5580a32a364f9112
Andrey Svishchev <andrey.svishchev@yandex.ru>
2021-12-17 20:51:40 +0300

    added tabs

---
+++ табы/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelector('.tabs');
+  const tabsBtn = document.querySelectorAll('.tabs__button');
+  const tabsContent = document.querySelectorAll('.tabs__content');
+
+  if (tabs) {
+    tabs.addEventListener('click', (e) => {
+      if (e.target.classList.contains('tabs__button')) {
+        const tabsPath = e.target.dataset.tabsPath;
+        tabsHandler(tabsPath);
+      }
+    })
+  }
+
+  const tabsHandler = (path) => {
+    tabsBtn.forEach(el => {
+      el.classList.remove('tabs__button--active')
+    });
+    document.querySelector(`[data-tabs-path="${path}"]`).classList.add('tabs__button--active');
+
+    tabsContent.forEach(el => {
+      el.classList.remove('tabs__content--active')
+    });
+    document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content--active');
+  }
+
+
+
+})
